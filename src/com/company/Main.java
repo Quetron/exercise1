08d081ef8f5db4202d6834766496486ed6ad934f
Quetron <060776@swcsd.us>
2022-01-20 14:26:06 -0500

    Name

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,8 @@
+/*
+Quintin Amon
+01/20/22
+Exercise 1
+*/
 package com.company;
 
 public class Main {
@@ -9,6 +14,6 @@ public class Main {
         double[] temps = {32.0, 25.0, 27.5, 40.0, 45.0};
         double avg = (temps[0]+temps[1]+temps[2]+temps[3]+temps[4])/5;
         System.out.print("City:" + city + " " + "Zip code:" + zip + " " + "Average temp:" + avg);
-        
+
     }
 }
