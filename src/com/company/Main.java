389929b466f3e41966428510fb45081578e85490
Quetron <060776@swcsd.us>
2022-01-20 14:24:08 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int zip = 43215;
+        String city = "Columbus";
+        double[] temps = {32.0, 25.0, 27.5, 40.0, 45.0};
+        double avg = (temps[0]+temps[1]+temps[2]+temps[3]+temps[4])/5;
+        System.out.print("City:" + city + " " + "Zip code:" + zip + " " + "Average temp:" + avg);
+        
+    }
+}
